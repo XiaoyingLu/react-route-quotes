5801582a53329188b9ca34c4339b0fefb349c125
Amber Lu <amber.lu@ubisoft.com>
2022-03-23 23:06:39 -0300

    route params, switch and redirect

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import { Route } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Products from "./pages/Products";
 import Welcome from "./pages/Welcome";
-import MainHeader from './components/MainHeader';
+import MainHeader from "./components/MainHeader";
+import ProductDetail from "./pages/ProductDetail";
 
 function App() {
   return (
     <div>
       <MainHeader />
       <main>
-        <Route path="/welcome">
-          <Welcome />
-        </Route>
-        <Route path="/products">
-          <Products />
-        </Route>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/welcome" />
+          </Route>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
