d1b7b12a05f0d6da6f994a17456278d56f60066d
Amber Lu <amber.lu@ubisoft.com>
2022-03-23 22:39:27 -0300

    nav between two pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { Route } from "react-router-dom";
+import Products from "./pages/Products";
+import Welcome from "./pages/Welcome";
+import MainHeader from './components/MainHeader';
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <MainHeader />
+      <main>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+      </main>
     </div>
   );
 }
