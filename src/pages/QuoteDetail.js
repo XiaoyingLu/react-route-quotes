711a42961290925380b1129799c3618c1418c3b2
Amber Lu <amber.lu@ubisoft.com>
2022-03-24 00:09:00 -0300

    "load comments" route in quoteDetail page

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,4 +1,4 @@
-import { useParams, Route } from "react-router-dom";
+import { useParams, Route, Link } from "react-router-dom";
 import { Fragment } from "react";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
@@ -18,6 +18,13 @@ const QuoteDetail = () => {
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}/`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
